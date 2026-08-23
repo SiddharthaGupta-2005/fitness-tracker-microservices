6ae64c28b6742768f00d9a2173508c019be055ea
Siddhartha <guptasiddhartha502@gmail.com>
2026-08-23 12:37:29 +0530

    fix(aiservice): use JacksonJavaTypeMapper instead of Jackson2JavaTypeMapper in RabbitMqConfig

--- aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.JacksonJavaTypeMapper;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,7 +41,7 @@ public class RabbitMqConfig {
     @Bean
     public MessageConverter jsonMessageConverter(){
         JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter();
-        converter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
+        converter.setTypePrecedence(JacksonJavaTypeMapper.TypePrecedence.INFERRED);
         return converter;
     }
 }
